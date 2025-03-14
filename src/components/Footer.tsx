3d85cce38b50bdadb43c5c522118ef17083d62ac
Jarkynbyek Japar <18748558+devsteppe9@users.noreply.github.com>
2025-03-13 20:06:53 -0500

    feat: update favicon to WebP format and enhance metadata in HTML and manifest

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,9 +7,20 @@ function Footer() {
   return (
     <footer>
       <div>
-        <a href="https://github.com/devsteppe9" target="_blank" rel="noreferrer"><GitHubIcon /></a>
-        <a href="https://www.linkedin.com/in/jarkynbyek-japar-83575712a/" target="_blank" rel="noreferrer"><LinkedInIcon /></a>
+        <a href="https://github.com/devsteppe9" target="_blank" rel="noreferrer noopener" title="GitHub Profile"><GitHubIcon /></a>
+        <a href="https://www.linkedin.com/in/jarkynbyek-japar-83575712a/" target="_blank" title="Linkedin Profile" rel="noreferrer noopener"><LinkedInIcon /></a>
       </div>
+      <p>
+        A special thanks to{" "}
+        <a
+          href="https://github.com/yujisatojr/react-portfolio-template"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Yuji Sato
+        </a>{" "}
+        for providing the original template!
+      </p>
     </footer>
   );
 }
